62ee34e88025ad9899fd7f2db00f7a243cb5a8cd
tippo <latifardiansyah982@gmail.com>
2024-01-21 22:31:23 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ['./index.html/**/*.{html,js}'],
   theme: {
     extend: {},
   },
